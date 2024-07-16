ef9a925011c4eacee3d6a1f599b7c0d43ae932a7
gmtstephane <gmtstephane@gmtstephanesmbp.home>
2024-07-16 09:58:40 +0200

    update nvim

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,16 +1,35 @@
-local configs = require("nvchad.configs.lspconfig")
-
-local on_attach = configs.on_attach
-local on_init = configs.on_init
-local capabilities = configs.capabilities
+local nvchad_on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
-local servers = { "html", "cssls", "gopls" }
 
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_init = on_init,
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
+-- Custom on_attach function
+local function custom_on_attach(client, bufnr)
+  -- Call the original on_attach function from NvChad
+  nvchad_on_attach(client, bufnr)
+
+  -- Additional key mapping for Telescope
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<Cmd>Telescope lsp_implementations<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>Telescope lsp_definitions<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", "<Cmd>Telescope lsp_document_symbols<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<Cmd>Telescope lsp_references<CR>", opts)
+
+-- map("n", "<leader>gi", ":Telescope lsp_implementations<CR>", { desc = "Telescope lsp_implementations" })
+-- map("n", "<leader>gd", ":Telescope lsp_definitions<CR>", { desc = "Telescope lsp_definitions" })
+-- map("n", "<leader>gs", ":Telescope lsp_document_symbols<CR>", { desc = "Telescope document symbols" })
+-- map("n", "<leader>gr", ":Telescope lsp_references<CR>", { desc = "Telescope lsp_references" })
 end
+
+local servers = { "gopls" }
+
+-- LSP setup with the custom on_attach function
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = custom_on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,39 +1,13 @@
-require("nvchad.mappings")
+require "nvchad.mappings"
 
 -- add yours here
 
 local map = vim.keymap.set
 
-map("n", "<leader>tt", ":GoAltV <CR>", { desc = "Go Toggle Test Vert" })
-map("n", "<leader>tF", ":GoTestFile -v -F <CR>", { desc = "GoTestFile" })
-map("n", "<leader>tf", ":GoTestFunc -v -F <CR>", { desc = "GoTestFunc" })
-
 map("n", ";", ":", { desc = "CMD enter command mode" })
-
-map("c", "Q", "q", { desc = "CMD enter command mode" })
-map("c", "Qa", "qa", { desc = "CMD enter command mode" })
-map("c", "qA", "qa", { desc = "CMD enter command mode" })
-map("c", "X", "x", { desc = "CMD enter command mode" })
-map("c", "W", "w", { desc = "CMD enter command mode" })
-
-map("n", "<leader>fm", function()
-	require("conform").format()
-end, { desc = "File Format with conform" })
-
-map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
-
--- local nomap = vim.keymap.del
--- nomap("n", "gi")
-map("n", "<leader>fi", ":Telescope lsp_implementations<CR>", { desc = "Telescope lsp_implementations" })
-map("n", "<leader>fd", ":Telescope lsp_definitions<CR>", { desc = "Telescope lsp_definitions" })
-map("n", "<leader>fs", ":Telescope lsp_document_symbols<CR>", { desc = "Telescope document symbols" })
-map("n", "<leader>fr", ":Telescope lsp_references<CR>", { desc = "Telescope lsp_references" })
-
-map("n", "<leader>gg", ":LazyGit <CR>", { desc = "LazyGit" })
-
+map("i", "jk", "<ESC>")
 map("n", "<C-d>", "<C-d>zz", { remap = true, desc = "Center cursor after moving down half-page" })
 map("n", "<C-u>", "<C-u>zz", { remap = true, desc = "Center cursor after moving up half-page" })
-
 map(
 	"n",
 	"]]",
@@ -47,3 +21,11 @@ map(
 	":TSTextobjectGotoPreviousStart @function.outer | lua vim.cmd('norm zz') <CR>",
 	{ remap = true, desc = "Go to previous function and center the cursor" }
 )
+
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
